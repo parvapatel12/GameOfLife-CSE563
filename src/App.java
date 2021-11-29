495417e2074bebfbc9375000bd5f0a8334990db9
arutselvan <arutselvan710@gmail.com>
2021-11-28 21:46:48 -0700

    Remove extra comments

--- src/App.java
+++ src/App.java
@@ -3,11 +3,9 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         
-    	// make window object
         BaseGUI GUI = new BaseGUI();
-        GUI.init(); // init all our things!
+        GUI.init();
 
-        // set window object size
         GUI.setSize(800, 800);
         GUI.setTitle("Game of Life -  Group 7 (CSE 563)");
         GUI.setVisible(true);
