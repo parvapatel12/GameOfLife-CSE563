00ddfb22239a0389dbb932ed4de511b4bc57ea01
Parva Patel <parvapatel@parvas-mbp.3500.dhcp.asu.edu>
2021-11-30 16:31:01 -0700

    UC1: Added game window, grid and menu to the game + Defined the data structures for grid and menu

--- src/App.java
+++ src/App.java
@@ -2,13 +2,11 @@
 import javax.swing.*;
 public class App {
     public static void main(String[] args) {
-        
-        BaseGUI GUI = new BaseGUI();
-        GUI.init();
-
-        GUI.setSize(800, 800);
-        GUI.setTitle("Game of Life -  Group 7 (CSE 563)");
-        GUI.setVisible(true);
-        GUI.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        BaseGUI gameWindow = new BaseGUI();
+        gameWindow.init();
+        gameWindow.setSize(800, 800);
+        gameWindow.setTitle("Game of Life -  Group 7 (CSE 563)");
+        gameWindow.setVisible(true);
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
